8d55b7ced69e68da071e646bedee2e2d3c7ed74b
okupooh <okupooh@gmail.com>
2021-10-09 12:01:26 +0900

    展開図カメラ位置修正

--- threebsp_js_test_surface.js
+++ threebsp_js_test_surface.js
@@ -15,8 +15,8 @@ var positionSurfaceView = [
 var positionSurfaceCamera = [
 	{ x: 0 , y: 0 , z: 1 },
 	{ x: 0 , y: 0 , z: -1 },
-	{ x: 0 , y: -1 , z: 0 },
 	{ x: 0 , y: 1 , z: 0 },
+	{ x: 0 , y: -1 , z: 0 },
 	{ x: 1 , y: 0 , z: 0 },
 	{ x: -1 , y: 0 , z: 0 },
 ];
