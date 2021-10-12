d93b5887de61afae4eaf6ccda88af2ad80210056
okupooh <okupooh@gmail.com>
2021-10-12 12:36:45 +0900

    衝突部分の描画

---
+++ bak/threebsp_js_test_surface_union_bak.js
@@ -0,0 +1,285 @@
+/* 面番号と位置：
+ *    2
+ *  5 0 4 1 
+ *    3
+ */
+var positionSurfaceView = [
+	{ top: 1 , left: 1 },
+	{ top: 1 , left: 3 },
+	{ top: 0 , left: 1 },
+	{ top: 2 , left: 1 },
+	{ top: 1 , left: 2 },
+	{ top: 1 , left: 0 },
+];
+
+var positionSurfaceCamera = [
+	{ x: 0 , y: 0 , z: 1 },
+	{ x: 0 , y: 0 , z: -1 },
+	{ x: 0 , y: 1 , z: 0 },
+	{ x: 0 , y: -1 , z: 0 },
+	{ x: 1 , y: 0 , z: 0 },
+	{ x: -1 , y: 0 , z: 0 },
+];
+
+
+function getSurfaceWidthHeight(  width , height , depth )
+{
+	const surfaceWH = [] ;
+
+	surfaceWH.push( { width: width , height: height } ) ;
+	surfaceWH.push( { width: width , height: height } ) ;
+	surfaceWH.push( { width: width , height: depth } ) ;
+	surfaceWH.push( { width: width , height: depth } ) ;
+	surfaceWH.push( { width: depth , height: height } ) ;
+	surfaceWH.push( { width: depth , height: height } ) ;
+
+	return surfaceWH ;
+}
+
+
+function getSurfacePosition( w , h , d )
+{
+	const surfacePos = [
+		{ left: d     , top: d   , width: w , height: h } ,
+		{ left: w+d*2 , top: d   , width: w , height: h } ,
+		{ left: d     , top: 0   , width: w , height: d } ,
+		{ left: d     , top: h+d , width: w , height: d } ,
+		{ left: w+d   , top: d   , width: d , height: h } ,
+		{ left: 0     , top: d   , width: d , height: h }
+	] ;
+
+	return surfacePos ;
+}
+
+
+function getSurfaceCameras( width , height , depth )
+{
+	const cameras = [] ;
+
+//	const surWH = getSurfaceWidthHeight(width , height , depth ) ;
+	const surPos = getSurfacePosition(width , height , depth ) ;
+	const far = 2000;
+	const ratio = 2;
+
+	for(let i=0 ; i<6 ; i++){
+	//	const aspect = surPos[i].width / surPos[i].height;
+		const aspect = 1 ;
+	const subcamera = new THREE.OrthographicCamera(
+			- ((surPos[i].width * aspect) / 2) , ((surPos[i].width * aspect) / 2) ,
+			(surPos[i].height / 2) , - (surPos[i].height / 2 ) , 0.1, far) ;		//(画角, アスペクト比)
+	//	const subcamera = new THREE.OrthographicCamera(
+	//		surWH[i].width * aspect / - ratio, surWH[i].width * aspect / ratio,
+	//		surWH[i].height / ratio, surWH[i].height / - ratio, 1, far) ;		//(画角, アスペクト比)
+		subcamera.position.set(	positionSurfaceCamera[i].x * width ,
+								positionSurfaceCamera[i].y * height ,
+								positionSurfaceCamera[i].z * depth ) ;	//カメラのセット位置（x, y, z）
+	//	subcamera.lookAt(scene.position);
+		subcamera.lookAt(new THREE.Vector3(0, 0, 0));
+		cameras.push(subcamera);
+	}
+
+	return cameras ;
+}
+
+
+function getSurfaceViewports( width , height , depth )
+{
+	const viewports = [] ;
+
+//	const surWH = getSurfaceWidthHeight(width , height , depth) ;
+	const surPos = getSurfacePosition( width , height , depth ) ;
+
+	for(let i=0 ; i<6 ; i++){
+		const area = {
+			left: Math.floor( surPos[i].left ) ,
+			top: Math.floor( surPos[i].top ) ,
+			width: Math.ceil( surPos[i].width ) ,
+			height: Math.ceil( surPos[i].height ) } ;
+
+//		renderer.setViewport( Math.floor(0 * width), Math.floor(0 * height),
+//							  Math.ceil(width), Math.ceil(height) );
+		/*
+		area.left = Math.floor(surPos[i].left) ;
+		area.top =  Math.floor(surPos[i].top) ;
+		area.width = Math.ceil( surPos[i].width ) ;
+		area.height = Math.ceil( surPos[i].height ) ;
+		*/
+
+		viewports.push(area);
+	}
+
+	return viewports ;
+}
+
+/*
+ *
+ *
+ */
+var _drills = [] ;
+
+function init( width , height , depth )
+{
+	//シーンを作成(シーンとは３D空間のこと,3Dオブジェクトそのものや光源の置き場)
+	const scene = new THREE.Scene({antialias: true});
+//	scene.background = new THREE.Color( 0xF0F0F0 );
+
+	//箱を作成
+	const meshbox = getBoxMesh(width, height, depth);
+
+	const box = new THREE.Mesh(geometry, material);
+	meshbox.position.set(0,0,0);
+
+	var resultBSP ;
+	var drillBSP ;
+	var unionBSP = null ;
+	var boxBSP = new ThreeBSP( box );
+
+
+	_drills.forEach( drl => {
+		const lathe = getDrillMeshCylinder(
+			new THREE.Vector3( drl.x1 , drl.y1 , drl.z1 ) ,
+			new THREE.Vector3( drl.x2 , drl.y2 , drl.z2 ) , drl.diameter , drl.tip ) ;
+	
+	//	scene.add(lathe);
+
+	//	drillBSP = new ThreeBSP( lathe );
+	//	boxBSP = new ThreeBSP( box );
+	
+	//	resultBSP = drillBSP.subtract( boxBSP ); 
+	//	resultBSP = boxBSP.intersect( drillBSP );
+	//	boxBSP = resultBSP ; 
+		if ( unionBSP == null ) {
+			unionBSP = new ThreeBSP( lathe );
+//console.log( `first` );
+		} else {
+			drillBSP = new ThreeBSP( lathe );
+			unionBSP = unionBSP.union( drillBSP ) ;
+		//	unionBSP = resultBSP ;
+//console.log( `union` );
+		}
+	});
+
+	resultBSP = boxBSP.subtract( unionBSP );
+	unionBSP = boxBSP.intersect( unionBSP );
+
+	const unionMesh = unionBSP.toMesh();
+/*
+	unionMesh.updateMatrix();
+	unionMesh.material.shading = THREE.FlatShading;
+	unionMesh.material.opacity = 0.0 ;
+	unionMesh.material.transparent = true ;
+	unionMesh.material.wireframe  = true ;
+	unionMesh.geometry.computeFaceNormals();
+	unionMesh.geometry.computeVertexNormals();
+	unionMesh.material.needsUpdate = true;
+	unionMesh.geometry.buffersNeedUpdate = true;
+	unionMesh.geometry.uvsNeedUpdate = true;
+//	scene.add( unionMesh );
+*/
+	const resultMesh = resultBSP.toMesh();
+	resultMesh.updateMatrix();
+
+	resultMesh.material.shading = THREE.FlatShading;
+//	resultMesh.material.shading = THREE.SmoothShading ;
+	resultMesh.material.opacity = 0.5 ;
+	resultMesh.material.transparent = true ;
+//	resultMesh.material.wireframe  = true ;
+//	resultMesh.material.depthTest = false ;
+	resultMesh.material.side = THREE.DoubleSide ;
+//	const plane = new THREE.Plane(new THREE.Vector3(0, 0, -1), - (depth /4) );
+//	resultMesh.material.clippingPlanes = [
+//			new THREE.Plane(new THREE.Vector3(0, 0, -1), Math.ceil(depth / 10)+1 ) ];
+
+	resultMesh.geometry.computeFaceNormals();
+    resultMesh.geometry.computeVertexNormals();
+    resultMesh.material.needsUpdate = true;
+    resultMesh.geometry.buffersNeedUpdate = true;
+    resultMesh.geometry.uvsNeedUpdate = true;
+//	resultMesh.updateMatrix();
+	
+	
+	const edges = new THREE.LineSegments(
+		new THREE.EdgesGeometry( unionMesh.geometry , 1 ) ,
+		new THREE.LineBasicMaterial( { color: 0xFFFFFF } ) 
+	) ;
+	unionMesh.add ( edges ) ;
+	
+	const boxedges = new THREE.LineSegments(
+		new THREE.EdgesGeometry( box.geometry , 1 ) ,
+		new THREE.LineBasicMaterial( { color: 0xFFFFFF } ) 
+	) ;
+	resultMesh.add ( boxedges ) ;
+
+	scene.add(resultMesh);
+//	scene.add(unionMesh);
+	scene.add(edges);
+
+
+	//	ライトの作成
+	// new THREE.AmbientLight(色, 光の強さ)
+//	const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
+	const light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	scene.add(light);
+
+	//	カメラの作成
+	const cameras = getSurfaceCameras( width , height , depth ) ;
+	const viewports = getSurfaceViewports( width , height , depth ) ;
+
+	const viewport3D = {
+		left: width*2 + depth*2 ,
+		top: 0 ,
+		width: height + depth*2 ,
+		height: height + depth*2 } ;
+
+	const camera3D = new THREE.PerspectiveCamera(45, 1, 0.1, 2000);
+//	const camera3D = new THREE.OrthographicCamera(
+//		( viewport3D.width  / 2) , ( viewport3D.width / -2 ) ,
+//		( viewport3D.height / 2) , ( viewport3D.height / -2 ) , 1, 2000) ;		//(画角, アスペクト比)
+
+	camera3D.position.set( width *2,  height *2, depth *2) ;	//カメラのセット位置（x, y, z）
+	camera3D.lookAt(scene.position);
+
+	//	レンダリング処理
+	const renderer = new THREE.WebGLRenderer({
+		canvas:document.querySelector('#myCanvas')
+	});
+	
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize((width*2 + depth*2)+viewport3D.width, height + depth*2);
+	renderer.autoClear = false ;
+	renderer.localClippingEnabled = true;
+
+	//	クリッピング平面
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(-1, 0, 0), Math.ceil(width / 2)+1 ) ) ;
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(1, 0, 0), Math.ceil(width / 2)+1 ) ) ;
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(0, -1, 0), Math.ceil(height / 2)+1 ) ) ;
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(0, 1, 0), Math.ceil(height / 2)+1 ) ) ;
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(0, 0, -1), Math.ceil(depth / 2)+1 ) ) ;
+	renderer.clippingPlanes.push(new THREE.Plane(new THREE.Vector3(0, 0, 1), Math.ceil(depth / 2)+1 ) ) ;
+
+	const controls = new THREE.OrbitControls(camera3D , document.querySelector('#myCanvas') );
+
+	//	初回実行
+	tick();
+
+	function tick(){
+		for(let i=0 ; i<6 ; i++){
+		//	cameras[i].lookAt(scene.position);
+			renderer.setViewport( viewports[i].left , viewports[i].top ,
+								  viewports[i].width , viewports[i].height );
+			renderer.render(scene, cameras[i]);//レンダリング（更新処理）
+		}
+
+		renderer.setViewport( viewport3D.left , viewport3D.top ,
+							  viewport3D.width , viewport3D.height );
+		renderer.render(scene, camera3D) ;
+		
+		controls.update();
+		requestAnimationFrame(tick);
+
+		//アニメーション処理をここに書く
+		// box.rotation.y += 0.01;
+		//renderer.render(scene, camera);//レンダリング（更新処理）
+	}
+
+}
---
+++ test_002/threebsp_js_test_mesh_union.js
@@ -0,0 +1,120 @@
+/*
+ *	箱 mesh を作成
+ */
+function getBoxMesh( wid , hei , dep )
+{
+	//箱を作成
+	/**立方体はメッシュと言う表示オブジェクトを使って描画する。
+	 *	メッシュを作るには、ジオメトリ（形状）とマテリアル（素材）の２種類を用意する必要がある 
+	 */
+	const gbox = new THREE.BoxGeometry( wid, hei, dep ) ;	//ner THREE.BoxGeometry(幅、高さ、奥行き)
+	const mat = new THREE.MeshBasicMaterial( {wireframe: true} );
+ 	const mbox = new THREE.Mesh(gbox, mat);
+//	mbox.position.set(0,0,0);
+
+	return mbox ;
+}
+
+
+/*
+ *	円+と円柱でドリル mesh を作成
+ */
+function getDrillMeshCylinder( start , end , diameter , tip )
+{
+	const vec3 = end.clone() ;
+	vec3.sub( start )  ;
+	const hei = vec3.length() ;
+	const dep = hei + tip ;
+	const rad = diameter / 2 ;
+
+//	const mat = new THREE.MeshBasicMaterial({opacity: 0.5, wireframeLinewidth: 0.5});
+	const mat = new THREE.MeshBasicMaterial( {wireframe: true} );
+
+	const gcon = new THREE.CylinderGeometry( 0 , rad , tip , 24 , false ) ;
+	// 円柱の座標は中心点が基準となるため2倍の長さを持つようにしている
+	const gcyl = new THREE.CylinderGeometry( rad , rad , hei * 2 , 24 , false ) ;                      
+
+	const mcon = new THREE.Mesh( gcon , mat ) ;
+	mcon.position.y = + ( hei + (tip/2) ) ;
+//	mcon.updateMatrix();
+//	gcyl.merge( mcon.geometry, mcon.matrix ) ;
+
+	const mcyl_w = new THREE.Mesh( gcyl , mat ) ;
+
+	const bcon = new ThreeBSP( mcon );
+	const bcyl_w = new ThreeBSP( mcyl_w );
+	const bcyl = bcyl_w.union( bcon ) ;
+	const mcyl = bcyl.toMesh() ;
+
+	vec3.normalize()
+	const axis = new THREE.Vector3(0, 1, 0);
+//	mcyl.position.y = + dep ;
+	mcyl.quaternion.setFromUnitVectors(axis, vec3);
+
+	mcyl.position.set( start.x , start.y , start.z ) ;
+/*
+	vec3.multiplyScalar( dep / 2 );
+	const pos = start.clone() ;
+	pos.add( vec3 ) ;
+	mcyl.position.set( pos.x , pos.y , pos.z ) ;
+*/
+	return mcyl ;
+}
+
+
+/*
+ *	結合した mesh を作成
+ */
+function getUnionMesh( meshes )
+{
+	var unionBSP = null ;
+
+	meshes.forEach( mesh => {
+		if ( unionBSP == null ) {
+			unionBSP = new ThreeBSP( mesh );
+		} else {
+			const meshBSP = new ThreeBSP( mesh );
+			unionBSP = unionBSP.union( meshBSP ) ;
+		}
+	});
+
+	const unionMesh = unionBSP.toMesh() ;
+
+	return unionMesh ;
+}
+
+
+/*
+ *	箱の内側で重なり合った部分の mesh を作成
+ */
+function getCrossMeshGroupInBox( meshes , boxMesh )
+{
+	const ret = new THREE.Group() ;
+
+	const boxBSP = new ThreeBSP( boxMesh ) ;
+	const mat = new THREE.MeshBasicMaterial( {
+		 color: 0xFF0000 ,
+		 opacity: 0.5 ,
+		 transparent: true
+	  } );
+
+	for( let i=0; i<meshes.length; i++) {
+		for( let j=0; j<meshes.length; j++) {
+			if ( i == j ) continue ;
+
+		//	const meshBSP1 = new ThreeBSP( meshes[i] ) ;
+		//	const meshBSP2 = new ThreeBSP( meshes[j] ) ;
+			const meshBSP1 = boxBSP.intersect( new ThreeBSP( meshes[i] ) ) ;
+			const meshBSP2 = boxBSP.intersect( new ThreeBSP( meshes[j] ) ) ;
+			const interBoxBSP = meshBSP1.intersect( meshBSP2 ) ;
+		//	const interBoxBSP = boxBSP.intersect( interBSP ) ;
+				
+			if ( interBoxBSP.toGeometry().vertices.length > 0 ) {
+				ret.add( interBoxBSP.toMesh( mat ) ) ; 				
+			} 
+		}
+	}
+
+	return ret ;
+}
+
